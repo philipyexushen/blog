02fd5017b22b9a4f1fedd2784b1f211343c5fab1
philipyexushen <defphilip@foxmail.com>
2018-03-22 19:25:36 +0800

    update

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myblog.settings")
 
 application = get_wsgi_application()
